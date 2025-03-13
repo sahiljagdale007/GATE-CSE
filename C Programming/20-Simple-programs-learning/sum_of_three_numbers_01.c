eb277f2c63504d4eee94f4d74daaa73ad14d0f6e
Sahil <sahilashokjagdale@gmail.com>
2025-03-13 06:47:34 +0530

    init commit

---
+++ C Programming/20-Simple-programs-learning/sum_of_three_numbers_01.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main() {
+    int a,b,c;
+    printf("Enter a number : ");
+    scanf("%d",&a);
+    printf("Enter a second number : ");
+    scanf("%d",&b);
+    c = a + b;
+    printf("The sum of two number is %d",c);
+}
